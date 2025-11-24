b337d1545382682d6b6598bfeb3bd0317f1a9533
beomly <ssung0305@gmail.com>
2025-11-24 21:13:04 +0900

    I did P1 and P2

--- HW4/HW4_S20221080_P1.cpp
+++ HW4/HW4_S20221080_P1.cpp
@@ -29,7 +29,7 @@ int main(){
     // x, y 문자열
     vector<char> x(len_x+1);
     vector<char> y(len_y+1);
-    // x, y 문자열 체크배열. 0: gap, 1: mismatch, 2: match
+    // x, y 문자열 체크배열. gap: 1: mismatch: 2: match: 3
     vector<int> x_check(len_x+1, 0);
     vector<int> y_check(len_y+1, 0);
     for (int i = 1; i <= len_x; i++){
@@ -39,7 +39,8 @@ int main(){
         read_file.read(reinterpret_cast<char*>(&y[i]), sizeof(char));
     }
     read_file.close();
-    int score1, score2, score3, temp, one, two, three;
+
+    int iandj, ygap, xgap, best;
     bool is_match;
     // 계산 과정
     vector<vector<int>> mat(len_x+1, vector<int>(len_y+1,0));
@@ -55,54 +56,91 @@ int main(){
             }
             else{
                 if (x[i]==y[j]){
-                    score1 = mat[i-1][j-1] + s; // x의 i와 y의 j가 일치
+                    iandj = mat[i-1][j-1] + s; // x의 i와 y의 j가 일치
                     is_match = true;
                 }
                 else{
-                    score1 = mat[i-1][j-1] - f; // x의 i와 y의 j가 불일치
+                    iandj = mat[i-1][j-1] - f; // x의 i와 y의 j가 불일치
                     is_match = false;
                 }
-                score2 = mat[i][j-1] - p; // y에 gap 삽입
-                score3 = mat[i-1][j] - p; // x에 gap 삽입
-                one = score1;
-                two = score2;
-                three = score3;
-                if (one > two){
-                    temp = one;
-                    one = two;
-                    two = temp;
+                ygap = mat[i][j-1] - p; // y에 gap 삽입
+                xgap = mat[i-1][j] - p; // x에 gap 삽입
+                
+                best = iandj;
+                if (is_match){
+                    check[i][j] = 3; // match
                 }
-                if (two > three){
-                    temp = two;
-                    two = three;
-                    three = temp;
+                else{
+                    check[i][j] = 2; // mismatch
                 }
-                if (one > two){
-                    temp = one;
-                    one = two;
-                    two = temp;
-                } // one, two, three non-decreasing 정렬
-                if (score1 == three){
-                    if (is_match){
-                        check[i][j]=3;
-                    }
-                    else{
-                        check[i][j]=2;
-                    }
+                if (ygap > best){
+                    best = ygap;
+                    check[i][j] = 1;
                 }
-                else if (score2 == three){
-                    check[i][j]=1;
+                if (xgap > best){
+                    best = xgap;
+                    check[i][j] = 0;
                 }
-                else if (score3 == three){
-                    check[i][j]=0;
-                }
+                mat[i][j] = best; // 최댓값 저장
             }
         }
     }
-
-
+    int idx_i = len_x;
+    int idx_j = len_y;
+    int gap_count_x = 0;
+    int gap_count_y = 0;
+    int string_len = 0;
+    while (idx_i > 0 && idx_j > 0){
+        if (check[idx_i][idx_j] == 3 || check[idx_i][idx_j] == 2){ // match or mismatch
+            x_check[idx_i] = check[idx_i][idx_j];
+            y_check[idx_j] = check[idx_i][idx_j];
+            idx_i--;
+            idx_j--;
+        }
+        else if (check[idx_i][idx_j] == 1){ // gap in y
+            y_check[idx_j] = 1;
+            idx_j--;
+            gap_count_y++;
+        }
+        else if (check[idx_i][idx_j] == 0){ // gap in x
+            x_check[idx_i] = 1;
+            idx_i--;
+            gap_count_x++;
+        }
+        string_len++;
+    }
+    while (idx_i > 0){
+        x_check[idx_i] = 1;
+        idx_i--;
+        gap_count_x++;
+        string_len++;
+    }
+    while (idx_j > 0){
+        y_check[idx_j] = 1;
+        idx_j--;
+        gap_count_y++;
+        string_len++;
+    }
     ofstream output_f;
     output_f.open(name_out);
-
+    if (!output_f.is_open()){
+        cout << "There is no file named " << name_out << '\n';
+        return 1;
+    }
+    output_f << mat[len_x][len_y] << '\n'; // 최대 점수
+    output_f << string_len << '\n'; // 정렬된 문자열 길이
+    output_f << gap_count_x << '\n'; // x의 gap 개수
+    for (int i=1; i<=len_x; i++){
+        if (x_check[i] == 1){
+            output_f << i << '\n';
+        }
+    } // x의 gap 위치
+    output_f << gap_count_y << '\n'; // y의 gap 개수
+    for (int i=1; i<=len_y; i++){
+        if (y_check[i] == 1){
+            output_f << i << '\n';
+        }
+    } // y의 gap 위치
+    output_f.close();
     return 0;
 }
