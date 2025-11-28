f3245da4d23e7d317141540355a4fc6948113dd9
Seongbeom Lee <ssung0305@gmail.com>
2025-11-28 17:00:09 +0900

    mm

--- HW4/Project1/HW4_S20221080_P2.cpp
+++ HW4/Project1/HW4_S20221080_P2.cpp
@@ -50,12 +50,9 @@ int main() {
         arr[0][0] = 1;
         arr[0][1] = 0;
         for (int i = 1; i < len; i++) {
-            arr[i][0] = i + 2; // 불가능한 값으로 초기화
-        }
-        for (int i = 1; i < len; i++) {
             min = i + 1; // 무조건 i+1보다는 작은 개수를 가지게 됨. i의 idx에서는 0~i까지 전부 회문에 속하지 않는, 전부 다른 숫자라고 했을 때의 개수가 i+1개임.
             min_idx = i; // arr[i][i+1]칸에 넣어놓을 i까지의 최소 회문개수가 되도록 하는 i가 포함될 회문의 시작점
-            for (int j = 1; j < i; j++) {
+            for (int j = 1; j <= i; j++) {
                 if (arr[i - 1][j] != i + 1 && s[j - 1] == s[i]) { // i-1행에서 i+1은 불가능한 개수.
                     arr[i][j - 1] = arr[j - 1][j - 1];
                     if (arr[i][j - 1] < min) {
@@ -66,16 +63,6 @@ int main() {
                 else {
                     arr[i][j-1] = i + 2; // 회문이 아닐 시 불가능한 값으로 초기화
                 }
-            }
-            if (s[i - 1] == s[i]) { // 바로 전 원소와 같다면 i~i-1까지도 회문
-                arr[i][i - 1] = arr[i - 1][i - 1];
-                if (arr[i][i - 1] < min) {
-                    min = arr[i][i - 1];
-                    min_idx = i - 1;
-                }
-            }
-            else{
-                arr[i][i - 1] = i+2;
             }
             arr[i][i] = arr[i - 1][arr[i - 1][i]] + 1;
             if (arr[i][i] < min) {
